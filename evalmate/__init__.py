d9bd94da0099449b786f0d6aa85cf9c4e3e5c9c7
buec <m.buechi@outlook.com>
2018-08-27 15:45:40 +0200

    Bump version: 0.1.0 → 0.2.0

--- evalmate/__init__.py
+++ evalmate/__init__.py
@@ -6,4 +6,4 @@ from evalmate.tasks.classification import ClassificationEvaluation  # noqa: F401
 from evalmate.tasks.kws import KWSEvaluation  # noqa: F401
 from evalmate.tasks.kws import KWSEvaluator  # noqa: F401
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
