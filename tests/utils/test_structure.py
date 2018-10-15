82908e08ecee0f47bc58690d083d8ae2e09c0c73
buec <m.buechi@outlook.com>
2018-10-15 21:39:48 +0200

    Some tests for Segment class

--- tests/utils/test_structure.py
+++ tests/utils/test_structure.py
@@ -2,6 +2,8 @@ from audiomate.corpus import assets
 
 from evalmate.utils import structure
 
+import pytest
+
 
 class TestLabelPair:
 
@@ -58,3 +60,77 @@ class TestLabelPair:
                                      assets.Label('b2', start=1.66, end=1.92))
 
         assert pair_a < pair_b
+
+
+class TestSegment:
+
+    def test_duration(self):
+        seg = structure.Segment(0.8, 1.9)
+
+        assert seg.duration == pytest.approx(1.1)
+
+    def test_compare_single_labels_with_same_times_returns_smaller_label_value(self):
+        seg_a = structure.Segment(0.0, 1.0, ref=assets.Label('a'))
+        seg_b = structure.Segment(0.0, 1.0, ref=assets.Label('b'))
+
+        assert seg_a < seg_b
+
+    def test_compare_single_labels_returns_smaller_end_time(self):
+        seg_a = structure.Segment(0.0, 0.8, ref=assets.Label('a'))
+        seg_b = structure.Segment(0.0, 0.9, ref=assets.Label('a'))
+
+        assert seg_a < seg_b
+
+    def test_compare_single_labels_returns_smaller_start_time(self):
+        seg_a = structure.Segment(0.0, 0.9, ref=assets.Label('a'))
+        seg_b = structure.Segment(0.2, 0.7, ref=assets.Label('a'))
+
+        assert seg_a < seg_b
+
+    def test_compare_multi_labels_with_same_times_returns_smaller_label_value(self):
+        seg_a = structure.Segment(0.0, 1.0, ref=[
+            assets.Label('a'),
+            assets.Label('a')
+        ])
+        seg_b = structure.Segment(0.0, 1.0, ref=[
+            assets.Label('a'),
+            assets.Label('b')
+        ])
+
+        assert seg_a < seg_b
+
+    def test_compare_multi_labels_returns_smaller_end_time(self):
+        seg_a = structure.Segment(0.0, 0.9, ref=[
+            assets.Label('a'),
+            assets.Label('a')
+        ])
+        seg_b = structure.Segment(0.0, 1.0, ref=[
+            assets.Label('a'),
+            assets.Label('b')
+        ])
+
+        assert seg_a < seg_b
+
+    def test_compare_multi_labels_returns_smaller_start_time(self):
+        seg_a = structure.Segment(0.0, 0.9, ref=[
+            assets.Label('a'),
+            assets.Label('a')
+        ])
+        seg_b = structure.Segment(0.2, 0.8, ref=[
+            assets.Label('a'),
+            assets.Label('a')
+        ])
+
+        assert seg_a < seg_b
+
+    def test_equals(self):
+        seg_a = structure.Segment(0.0, 0.9, ref=[
+            assets.Label('a'),
+            assets.Label('a')
+        ])
+        seg_b = structure.Segment(0.0, 0.9, ref=[
+            assets.Label('a'),
+            assets.Label('a')
+        ])
+
+        assert seg_a == seg_b
